de9d6a8c6bb2fee5a03564c8d754f0329bdb82f3
Joshi Sourabha <sourabhjoshi124@gmail.com>
2025-12-07 13:12:08 +0530

    text interpolation

---
+++ projects/UsingVITE/my-vue-excersises/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import BasicExample from "./components/TextInterpolation/BasicExample.vue"
+</script>
+
+<template>
+  <BasicExample/>
+</template>
+
---
+++ projects/UsingVITE/my-vue-excersises/src/components/TextInterpolation/BasicExample.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref} from "vue";
+
+const name = ref('Joshi Sourabha')
+const age = ref(25)
+</script>
+
+<template>
+  <p>Text Interpolation : </p>
+  <p>name : {{ name }}</p>
+  <p>Age : {{ age }}</p>
+  <p>Expression : {{ 2 + 5 * 8 }}</p>
+  <p>Next year your age : {{age+1}}</p>
+</template>
